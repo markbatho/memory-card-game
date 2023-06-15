2458ff6d959503200bf131283a950742dacddb86
Mark Batho <mark747488@gmail.com>
2023-06-15 14:19:56 +0200

    Add flag assets and flag data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
+const flags = [
+  { country: 'Argentina', flag: 'ar' },
+  { country: 'Austria', flag: 'at' },
+  { country: 'Belgium', flag: 'be' },
+  { country: 'Bulgaria', flag: 'bg' },
+  { country: 'Brazil', flag: 'br' },
+  { country: 'Canada', flag: 'ca' },
+  { country: 'Switzerland', flag: 'ch' },
+  { country: 'China', flag: 'cn' },
+  { country: 'Cuba', flag: 'cu' },
+  { country: 'Germany', flag: 'de' },
+  { country: 'Egypt', flag: 'eg' },
+  { country: 'Finland', flag: 'fi' },
+  { country: 'Greece', flag: 'gr' },
+  { country: 'Hungary', flag: 'hu' },
+  { country: 'Israel', flag: 'il' },
+  { country: 'India', flag: 'in' },
+  { country: 'Italy', flag: 'it' },
+  { country: 'Japan', flag: 'jp' },
+  { country: 'South Korea', flag: 'kr' },
+  { country: 'Pakistan', flag: 'pk' },
+  { country: 'Poland', flag: 'pl' },
+  { country: 'Portugal', flag: 'pt' },
+  { country: 'Serbia', flag: 'rs' },
+  { country: 'San Marino', flag: 'sm' },
+  { country: 'USA', flag: 'us' },
+];
+
 function App() {
   return (
     <div className="App">
